1011107c1d2eb83876c203ed9c26076baeaf9f70
Farnaz Baksh <farnaz.baksh@gmail.com>
2018-04-16 02:16:38 -0400

    Final 0.5

---
+++ main.js
@@ -0,0 +1,59 @@
+function ingred11() {
+  var x=document.getElementById("ingred1");
+  if (x.style.display=="none") {x.style.display="block";}
+    else {x.style.display="none"}
+}
+
+function ingred22() {
+  var x=document.getElementById("ingred2");
+  if (x.style.display=="none") {x.style.display="block";}
+    else {x.style.display="none"}
+}
+
+function ingred33() {
+  var x=document.getElementById("ingred3");
+  if (x.style.display=="none") {x.style.display="block";}
+    else {x.style.display="none"}
+}
+
+function ingred44() {
+  var x=document.getElementById("ingred4");
+  if (x.style.display=="none") {x.style.display="block";}
+    else {x.style.display="none"}
+}
+
+function ingred55() {
+  var x=document.getElementById("ingred5");
+  if (x.style.display=="none") {x.style.display="block";}
+    else {x.style.display="none"}
+}
+
+function ingred66() {
+  var x=document.getElementById("ingred6");
+  if (x.style.display=="none") {x.style.display="block";}
+    else {x.style.display="none"}
+}
+
+function ingred77() {
+  var x=document.getElementById("ingred7");
+  if (x.style.display=="none") {x.style.display="block";}
+    else {x.style.display="none"}
+}
+
+function ingred88() {
+  var x=document.getElementById("ingred8");
+  if (x.style.display=="none") {x.style.display="block";}
+    else {x.style.display="none"}
+}
+
+function ingred99() {
+  var x=document.getElementById("ingred9");
+  if (x.style.display=="none") {x.style.display="block";}
+    else {x.style.display="none"}
+}
+
+function ingred100() {
+  var x=document.getElementById("ingred10");
+  if (x.style.display=="none") {x.style.display="block";}
+    else {x.style.display="none"}
+}
